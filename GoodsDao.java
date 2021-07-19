20c06435fc12e67d946d9722f06295114db959f6
eewonjong <lwj960910@naver.com>
2021-07-19 14:36:39 +0900

    goodsDao 생성 [이원종]

---
+++ GoodsDao.java
@@ -0,0 +1,13 @@
+class GoodsDao
+{
+	public static void main(String[] args) 
+	{
+		public void insert(){
+			System.out.println("상품 등록");
+		}
+
+		public void delete(){
+			System.out.println("상품 삭제");
+		}
+	}
+}
