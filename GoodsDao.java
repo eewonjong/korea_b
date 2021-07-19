966df1e05b81d33cddfdd3ebefd22f9657a01f24
eewonjong <lwj960910@naver.com>
2021-07-19 14:53:15 +0900

    GoodsDao 충돌제거[이원종]

--- GoodsDao.java
+++ GoodsDao.java
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-class GoodsDao
-{
-	public static void main(String[] args) 
-	{
-		public void insert(){
-			System.out.println("상품 등록");
-		}
 
-		public void delete(){
-			System.out.println("상품 삭제");
-		}
-=======
 class GoodsDao 
 {
 	public void insertGoods(){
@@ -19,6 +7,10 @@ class GoodsDao
 
 	public void updateGoods(){
 		System.out.println("수정하였습니다.");
->>>>>>> 4322ca22e385e332a96dfb61f036608fe90fe0a1
+	}
+
+	
+	public void delete(){
+		System.out.println("상품 삭제");
 	}
 }
